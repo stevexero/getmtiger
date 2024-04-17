ff094de1bd4b655de72fcd846aab1c6cb6a44c9f
Steven Woodward <steveanthony999@gmail.com>
2024-04-16 19:10:58 -0700

    Updated add user tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 from flask_testing import TestCase
+from unittest.mock import patch
 from app import create_app
 from services.temp_service import generate_temp_token
 
@@ -29,7 +30,14 @@ class MyTest(TestCase):
         self.assertIn('pageTitle', response.json)
         self.assertIn('Browser automation successful', response.json['message'])
 
-    def test_add_user(self):
+    @patch('services.temp_service.decode_clerk_token')
+    @patch('services.user_service.add_user_to_database')
+    # def test_add_user(self):
+    def test_add_user(self, mock_add_user_to_database, mock_decode_clerk_token):
+        # Setup mocks
+        mock_decode_clerk_token.return_value = ('testuser', None)
+        mock_add_user_to_database.return_value = ({"user_id": "testuser", "email": "testuser@example.com"}, None, 201)
+
         token = generate_temp_token('testuser')
         user_data = {
             "user_id": "testuser",
@@ -39,10 +47,19 @@ class MyTest(TestCase):
         # First request to add a new user
         headers = {'Authorization': f'Bearer {token}'}
         response = self.client.post('/api/add-user', json=user_data, headers=headers)
+
         self.assertEqual(response.status_code, 201)
 
+        # Verify that the add user function was called with the correct data
+        mock_add_user_to_database.assert_called_once_with(user_data)
+
         # Second request with the same user data
+        # response = self.client.post('/api/add-user', json=user_data, headers=headers)
+        # self.assertNotEqual(response.status_code, 201, "Should not succeed")
+        # self.assertTrue('error' in response.json, "Should return error message")
+        # self.assertEqual(response.status_code, 409, "Should return a conflict status code (user already exists)")
+        mock_add_user_to_database.return_value = (None, 'User with this ID or Email already exists', 409)
         response = self.client.post('/api/add-user', json=user_data, headers=headers)
-        self.assertNotEqual(response.status_code, 201, "Should not succeed")
-        self.assertTrue('error' in response.json, "Should return error message")
-        self.assertEqual(response.status_code, 409, "Should return a conflict status code (user already exists)")
+        self.assertEqual(response.status_code, 409)
+        self.assertTrue('error' in response.json)
+        self.assertEqual(response.json['error'], 'User with this ID or Email already exists')
