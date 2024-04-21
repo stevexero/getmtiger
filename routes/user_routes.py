719420e7f73be92ac24e23d9d19fd05c2c6c3917
Steven Woodward <steveanthony999@gmail.com>
2024-04-21 16:29:33 -0700

    Adding add user manually script

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -65,3 +65,25 @@ def get_current_user():
         return jsonify({'error': str(error)}), status_code
 
     return jsonify(user_data), status_code
+
+
+@user_bp.route('/api/add-user-manually', methods=['POST'])
+def add_user_manually():
+    token = request.headers.get('Authorization', '').split(' ')[1] if 'Authorization' in request.headers else None
+    if not token:
+        return jsonify({'error': 'Authorization token is missing or invalid'}), 401
+    print('from get_current_user')
+    print(token)
+
+    user_id, error = decode_clerk_token(token)
+    if error:
+        return jsonify({'error': error}), 403
+
+    print('from get_current_user')
+    print("User ID retrieved:", user_id)
+
+    user_data, error, status_code = get_user_from_database(user_id)
+    print('from get_current_user')
+    print(user_data)
+
+#     if user role is, read from body the user inputs
