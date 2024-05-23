0096106d0eca40ae94805e7b5d38ef256f658356
Steven Woodward <steveanthony999@gmail.com>
2024-05-22 18:01:23 -0700

    Moved create app function

--- app.py
+++ app.py
@@ -26,8 +26,9 @@ def create_app():
     return app
 
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='0.0.0.0', port=5001, debug=True)
 
 # @app.route('/api/items/get-all', methods=['GET'])
